87561befcec5431cedbc9cce0bbc03f08bf33f56
wujingquan <i@wujingquan.com>
2019-07-16 22:41:48 +0800

    add favor table

---
+++ src/entities/favor.entity.ts
@@ -0,0 +1,16 @@
+import { Entity, Column, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class Favor {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  uid: number;
+  
+  @Column()
+  art_id: number;
+
+  @Column()
+  type: number;
+}
