8d763718899b51de49f5f7281fc081946c565eb6
wujingquan <i@wujingquan.com>
2019-07-19 23:58:16 +0800

    temp commit

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,7 +1,14 @@
-import { Controller, Get, UnauthorizedException } from '@nestjs/common';
+import { Controller, Get, UnauthorizedException, Post } from '@nestjs/common';
 import { AppService } from './app.service';
+import { AuthService } from './auth/auth.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService, private readonly authService: AuthService) {}
+  // constructor(private readonly authService: AuthService) {}
+  
+  @Post('login')
+  async login() {
+    return this.authService.login();
+  }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ClassicModule } from './classic/classic.module';
 import { LikeModule } from './like/like.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(), ClassicModule, LikeModule],
+  imports: [TypeOrmModule.forRoot(), ClassicModule, LikeModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
